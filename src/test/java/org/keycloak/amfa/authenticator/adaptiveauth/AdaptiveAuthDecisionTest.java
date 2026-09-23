9c81bb433e898655dddb99264c6cfe67e41dee3f
klajd <klajd@defensepoint.com>
2026-09-23 11:03:37 +0200

    Add SPDX headers to test files and pom.xml metadata
    Add SPDX-License-Identifier: Apache-2.0 headers to all 7 test files
    for consistency with the main source files.
    
    Add organization, scm, and issueManagement sections to pom.xml.
    
    Signed-off-by: klajd <klajd@defensepoint.com>

--- src/test/java/org/keycloak/amfa/authenticator/adaptiveauth/AdaptiveAuthDecisionTest.java
+++ src/test/java/org/keycloak/amfa/authenticator/adaptiveauth/AdaptiveAuthDecisionTest.java
@@ -1,3 +1,6 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.keycloak.amfa.authenticator.adaptiveauth;
 
 import org.junit.jupiter.api.Test;
--- src/test/java/org/keycloak/amfa/authenticator/mfa/MfaUtilsTest.java
+++ src/test/java/org/keycloak/amfa/authenticator/mfa/MfaUtilsTest.java
@@ -1,3 +1,6 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.keycloak.amfa.authenticator.mfa;
 
 import org.junit.jupiter.api.BeforeEach;
